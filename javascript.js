9065dd0d30ab695baf2834cfd659c30308460505
Projektdb <project.db@gmail.com>
2022-07-25 19:39:27 -0500

    Finish RPS Project
    - Screwed up my directory when starting over after a failed first attempt. Committing the finished version here. Gotta get better at version control.

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+
+
+
+function getComputerChoice() {
+    const moves = ["Rock", "Paper", "Scissors"];
+    let computerMove = moves[(Math.random() * moves.length)]
+    console.log(computerMove);
+}
+
+
+
+
+    
